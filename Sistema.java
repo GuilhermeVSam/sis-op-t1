ac6bc2f4e28d23372ec00e40f3123bb3ad3f19fc
fxgvcv <gabriel.fogaca@edu.pucrs.br>
2025-04-29 17:39:34 -0300

    Implement memory deallocation and clear memory utility in GM and GP classes

--- Sistema.java
+++ Sistema.java
@@ -676,6 +676,22 @@ public class Sistema {
             }
         }
 
+		public void clearMemory(int allocatedPages[], int pageSize) {
+			for (int pageFrame : allocatedPages) {
+                if (pageFrame < 0 || pageFrame >= (hw.mem.pos.length / pageSize)) {
+                    throw new IllegalArgumentException("Invalid page frame: " + pageFrame);
+                }
+				int physicalStart = pageFrame * pageSize;
+				int physicalEnd = physicalStart + pageSize;
+
+				for (int j = physicalStart; j < physicalEnd; j++) {
+					hw.mem.pos[j].opc = Opcode.___;
+					hw.mem.pos[j].ra = -1;
+					hw.mem.pos[j].rb = -1;
+					hw.mem.pos[j].p = -1;
+				}
+		}
+
 /*		private void loadAndExec(Word[] p) {
 			loadProgram(p, ); // carga do programa na memoria
 			System.out.println("---------------------------------- programa carregado na memoria");
